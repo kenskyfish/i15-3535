f35e9c7c83783d54dc66c7c3de8f1075e70e31ef
Kenneth Marsh <kenskyfish@gmail.com>
2024-10-25 13:29:33 +0200

    improved pipeline to accomodate postgresql

--- pipelines/jenkins/scaledeployments.groovy
+++ pipelines/jenkins/scaledeployments.groovy
@@ -12,20 +12,33 @@ pipeline {
                 container('k8s') {
                     withKubeConfig([credentialsId: 'Kubeconfig001']) {
                         script {
+                            replicas = '0'
+                            deployment = 'true'
+                            statefulset = 'false'
+                            monitoring = 'false'
+                            if ((env.NAMESPACE.indexOf('wiki') > -1) || (env.NAMESPACE.indexOf('harbor') > -1) || (env.NAMESPACE.indexOf('monitoring') > -1)) {
+                                statefulset = 'true'
+                            }
+                            if ((env.NAMESPACE.indexOf('postgresql') > -1) ) {
+                                deployment = 'false'
+                                statefulset = 'true'
+                            }
+                            if (env.NAMESPACE.indexOf('monitoring') > -1) {
+                                monitoring = 'true'
+                            }
                             if (env.UP == 'true') {
-                                sh 'kubectl scale deployments -n $NAMESPACE --replicas=1 --all'
-                                if ((env.NAMESPACE.indexOf('wiki') > -1) || (env.NAMESPACE.indexOf('monitoring') > -1) || (env.NAMESPACE.indexOf('harbor') > -1)) {
-                                    sh 'kubectl scale statefulsets -n $NAMESPACE --replicas=1 --all'
-                                }
-                                if (env.NAMESPACE.indexOf('monitoring') > -1) {
+                                replicas = '1'
+                            }
+                            if (deployment == 'true') {
+                                sh "kubectl scale deployments -n $NAMESPACE --replicas=$replicas --all"
+                            }
+                            if (statefulset == 'true') {
+                                sh "kubectl scale statefulsets -n $NAMESPACE --replicas=$replicas --all"
+                            }
+                            if (monitoring == 'true') {
+                                if (replicas == '1') {
                                     sh 'kubectl -n monitoring patch daemonset stack-prometheus-node-exporter --type json -p=\'[{\"op\": \"remove\", \"path\": \"/spec/template/spec/nodeSelector/non-existing\"}]\''
-                                }
-                        } else {
-                                sh 'kubectl scale deployments -n $NAMESPACE --replicas=0 --all'
-                                if ((env.NAMESPACE.indexOf('wiki') > -1) || (env.NAMESPACE.indexOf('monitoring') > -1) || (env.NAMESPACE.indexOf('harbor') > -1)) {
-                                    sh 'kubectl scale statefulsets -n $NAMESPACE --replicas=0 --all'
-                                }
-                                if (env.NAMESPACE.indexOf('monitoring') > -1) {
+                                } else {
                                     sh 'kubectl -n monitoring patch daemonset stack-prometheus-node-exporter -p \'{\"spec\": {\"template\": {\"spec\": {\"nodeSelector\": {\"non-existing\": \"true\"}}}}}\''
                                 }
                             }
